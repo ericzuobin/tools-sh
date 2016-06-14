39a2f18b45c8ece72259677033a0ef4f4221b0d7
ericzuobin <ericzuobin@qq.com>
2016-06-14 10:10:12 +0800

    提交ssh expect脚本

---
+++ sh/sshexpect.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/expect
+set timeout 10
+set host 172.16.3.81
+set name root
+set password admin
+
+set domain 172.16.3.
+set ip [lindex $argv 0]
+set name2 root
+set password2 admin
+
+spawn ssh $name@$host
+expect {
+    "(yes/no)?" {
+        send {"yes\n"; exp_continue}
+    }
+    "assword:" {
+        send "$password\n"
+    }
+    "*~]*" {
+      send "ssh $name2@$domain$ip\n"
+        expect {
+            "(yes/no)?" {
+                send {"yes\n"; exp_continue}
+            }
+            "assword:" {
+                send "$password2\n"
+           }
+           "*~]*" {exp_continue}
+      }
+   }
+}
+interact
