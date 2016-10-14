38bc64c25d270fa491f054092bd34b4acd1973b4
ericzuobin <ericzuobin@qq.com>
2016-10-14 11:16:12 +0800

    完善脚本

--- sh/jenkins_deploy.sh
+++ sh/jenkins_deploy.sh
@@ -3,15 +3,15 @@
 #在jenkins中 使用ssh root执行即可
 #配置环境
 job_home="/var/lib/jenkins/jobs/yygengine/workspace"
-base_home="/conf/yyg-engine"
 targetFile=$job_home/target/ROOT.war
 proc_name="dev-engine"
+base_home="/conf/yyg-engine"
 bak_home=$base_home/ROOT
 deploy_file="pengine-1.0-SNAPSHOT.war"
 tomcat_base="/opt/deploy"
 tomcat_version="apache-tomcat-7.0.68"
 
-#jenkins打包备份
+#检查是否Compile完成
 if [ -f $job_home/target/$deploy_file ];then
   echo "Compile Success,File Exits!"
 else
@@ -19,6 +19,7 @@ else
   exit
 fi
 
+#备份之前的打包
 myFile=$bak_home/ROOT.war
 if [ -f "$myFile" ]; then
  mv $bak_home/ROOT.war $bak_home/ROOT-$(date +%Y%m%d-%H%M%S).war
@@ -36,13 +37,18 @@ fi
 #关闭程序
 $tomcat_base/$proc_name/$tomcat_version/bin/shutdown.sh -force
 
-#确保程序关闭成功
+if [ $? -eq 0 ];then
+     echo $proc_name" shutdown success ..."
+else
+     echo $proc_name" shutdown failed ..."
+fi
+
+#再次确保程序关闭成功
 engine_pids=`ps -ef | grep java | grep apache-tomcat | grep $proc_name | awk '{print $2}'`
 if [[ -z $engine_pids ]];then
     echo "The "$proc_name" is not running ! "
 else
-echo $proc_name " pids:"
-     echo ${engine_pids[@]}
+echo $proc_name " pids:" ${engine_pids[@]}
      echo "------kill the task!------"
      for id in ${engine_pids[*]}
      do
@@ -53,7 +59,6 @@ echo $proc_name " pids:"
        echo "kill -9" ${id}
 
        if [ $? -eq 0 ];then
-
             echo "task is killed ..."
        else
             echo "kill task failed "
@@ -62,21 +67,51 @@ echo $proc_name " pids:"
 fi
 
 #替换新war
-echo "Begin to replace the files! "
+echo "Begin to replace the war! "
 rm -rf $tomcat_base/$proc_name/ROOT/*
+if [ $? -eq 0 ];then
+     echo $tomcat_base/$proc_name/ROOT/ deleted!
+else
+     echo $tomcat_base/$proc_name/ROOT/ delete failed!
+     exit
+fi
+
 cp $bak_home/ROOT.war $tomcat_base/$proc_name/ROOT/
+
+if [ $? -eq 0 ];then
+     echo Copy ROOT.war to $tomcat_base/$proc_name/ROOT/ success!
+else
+     echo Copy ROOT.war to $tomcat_base/$proc_name/ROOT/ failed!
+     exit
+fi
+
 unzip -qq $tomcat_base/$proc_name/ROOT/ROOT.war -d $tomcat_base/$proc_name/ROOT/
-echo "ROOT.war install success! "
+
+if [ $? -eq 0 ];then
+     echo Unpack ROOT.war to $tomcat_base/$proc_name/ROOT/ success!
+else
+     echo Unpack ROOT.war to $tomcat_base/$proc_name/ROOT/ failed!
+     exit
+fi
+
 rm $tomcat_base/$proc_name/ROOT/ROOT.war
 
 #替换配置文件
-echo "Begin to replace the properties and soft links! "
+echo "Begin to comfigure the "$proc_name
 rm -rf $tomcat_base/$proc_name/ROOT/WEB-INF/classes/engine.properties
 cp /conf/yyg-engine/engine.properties $tomcat_base/$proc_name/ROOT/WEB-INF/classes/engine.properties
 #配置软连接
 rm -rf $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy/*.xml
-ln -s $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy-virtual-ticket/deploy-virtual-ticket.xml $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy/applicationContext-virtual-ticket.xml 
+ln -s $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy-virtual-ticket/deploy-virtual-ticket.xml $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy/applicationContext-virtual-ticket.xml
 ln -s $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy-scanner.xml $tomcat_base/$proc_name/ROOT/WEB-INF/classes/deploy/applicationContext-scanner.xml
+
+if [ $? -eq 0 ];then
+     echo Comfigure success!
+else
+     echo Comfigure failed!
+     exit
+fi
+
 
 #启动程序
 echo "Begine to start the "$proc_name" !!! "
